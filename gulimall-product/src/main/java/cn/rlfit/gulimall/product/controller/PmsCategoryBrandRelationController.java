8ed17480707bfb8a0f84c775ebbc43fb9180d732
sunjianrong <sunruolifeng@gmail.com>
2024-03-14 14:25:41 +0800

    feat: 配置网关

--- gulimall-product/src/main/java/cn/rlfit/gulimall/product/controller/PmsCategoryBrandRelationController.java
+++ gulimall-product/src/main/java/cn/rlfit/gulimall/product/controller/PmsCategoryBrandRelationController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.HashMap;
 
 /**
  * @author: sunjianrong
