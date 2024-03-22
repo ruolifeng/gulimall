f1655d1b681270192fe7b594c4d60b4d81b6e07d
sunjianrong <sunruolifeng@gmail.com>
2024-03-22 21:47:06 +0800

    feat: 规格参数

---
+++ gulimall-product/src/main/java/cn/rlfit/gulimall/product/controller/PmsAttrController.java
@@ -0,0 +1,15 @@
+package cn.rlfit.gulimall.product.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: sunjianrong
+ * @email: sunruolifeng@gmail.com
+ * @date: 22/03/2024 9:45 PM
+ */
+@RestController
+@RequestMapping("/product/attr")
+public class PmsAttrController {
+
+}
